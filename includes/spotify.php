e5b144c3520cb2dd78f01e3a5513f6ddbfb60abd
Cut0x <71967731+Cut0x@users.noreply.github.com>
2022-08-18 08:38:39 +0200

    Update spotify.php

--- includes/spotify.php
+++ includes/spotify.php
@@ -1,1 +1,9 @@
-<iframe scrolling="no" src="votre_lien_ici" data-testid="spotify-preview--iframe" height="392" style="border-radius: 30px; width: 100%;" class="MusicPreviewEmbed__IframeWrapper-sc-15o0inq-0 fgmrkY"></iframe>
+<iframe
+        scrolling="no"
+        src="votre_lien_ici"
+        data-testid="spotify-preview--iframe"
+        height="392"
+        style="border-radius: 30px; width: 100%;"
+        class="MusicPreviewEmbed__IframeWrapper-sc-15o0inq-0 fgmrkY"
+>
+</iframe>
