9023c15a29fb4a0d40b8c9be2a26c1bd60d51a47
Cut0x <71967731+Cut0x@users.noreply.github.com>
2022-08-18 22:45:34 +0200

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer style="text-align: center;">
+  Développé avec ❤️ par <a href="https://twitter.com/Cut0x_" style="color: cyan; text-decoration: none;" target="_blank" rel="noopener noreferrer">Cut0x</a> !
+</footer>
