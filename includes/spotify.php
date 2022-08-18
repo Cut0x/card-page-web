eaa46cedd4b6cf98beb49cf2c81e384783808725
Cut0x <71967731+Cut0x@users.noreply.github.com>
2022-08-18 08:39:08 +0200

    Update spotify.php

--- includes/spotify.php
+++ includes/spotify.php
@@ -1,6 +1,6 @@
 <iframe
         scrolling="no"
-        src="votre_lien_ici"
+        src="votre_lien_spotify_ici"
         data-testid="spotify-preview--iframe"
         height="392"
         style="border-radius: 30px; width: 100%;"
