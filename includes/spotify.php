cca07947fd158539ab1da50f7c947455dce8b408
Cut0x <71967731+Cut0x@users.noreply.github.com>
2022-08-18 08:37:51 +0200

    Update spotify.php

--- includes/spotify.php
+++ includes/spotify.php
@@ -1,3 +1,1 @@
-<!--
-        ICI, L'INFRAME DE VOTRE PROFILE SPOTIFY
--->
+<iframe scrolling="no" src="votre_lien_ici" data-testid="spotify-preview--iframe" height="392" style="border-radius: 30px; width: 100%;" class="MusicPreviewEmbed__IframeWrapper-sc-15o0inq-0 fgmrkY"></iframe>
