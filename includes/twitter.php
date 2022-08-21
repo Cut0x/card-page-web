0f8338e0504ce7c1d545373711e1153241ed6ddc
Cut0x <71967731+Cut0x@users.noreply.github.com>
2022-08-21 21:15:45 +0200

    Update twitter.php

--- includes/twitter.php
+++ includes/twitter.php
@@ -9,3 +9,11 @@ include('./config.php');
     Follow @<?php echo $twitter; ?>
 </a>
 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<a
+    href="https://twitter.com/intent/tweet?screen_name=<?php echo $twitter; ?>&ref_src=twsrc%5Etfw"
+    class="twitter-mention-button"
+    data-show-count="false">
+    Tweet to @<?php echo $twitter; ?>
+</a>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
