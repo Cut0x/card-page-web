46bcb91e28ec30a03e5db57c1f3f3134c7a2b4b1
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-26 17:07:41 +0200

    Up

--- src/js/config.js
+++ src/js/config.js
@@ -8,8 +8,14 @@ const config = {
         {
             id: "p1",
             img: "https://cdn.discordapp.com/attachments/914271938359210045/980928769588072478/LOGO-DEVORION-1.png",
-            url: "https://discord.com/",
-            name: "DevOron"
+            url: "https://discord.gg/Pgvwy28yu3",
+            name: "DevOrion"
+        },
+        {
+            id: "p2",
+            img: "https://cdn.discordapp.com/attachments/914271938359210045/1100799158417363004/IMG_0311.jpg",
+            url: "https://discord.gg/aTX6FP37pK",
+            name: "Le Monde de Cut0x"
         },
     ],
 
