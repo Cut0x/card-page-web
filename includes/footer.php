b5f16b09538b38bb992a4ae465980bb98876212a
Loïc VALENCE <loic.valence@lmon-dolto.loc>
2023-04-26 08:50:28 +0200

    Up

--- includes/footer.php
+++
@@ -1,3 +0,0 @@
-<footer style="text-align: center;">
-  Développé avec ❤️ par <a href="https://twitter.com/Cut0x_" style="color: cyan; text-decoration: none;" target="_blank" rel="noopener noreferrer">Cut0x</a> !
-</footer>
