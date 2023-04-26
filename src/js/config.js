10551b6af3989e3e287b8fee6c118aadb612fb89
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-26 10:24:45 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 const config = {
     USERNAME: "Username#0001",
     DESCRIPTION: "This is my webpage card ! Follow me on my social reseau !",
-    PICTURE: "/logo.png"
+    PICTURE: "url_image"
 };
