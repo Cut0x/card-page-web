2f3f3fa01b7d30ea25835e07196419588deee7b5
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-26 10:23:33 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,8 @@
-function getPicture() {
-    if (config.PICTURE.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
-        let hash = CryptoJS.MD5(config.PICTURE);
-        return `https://gravatar.com/avatar/${hash}?s=512&d=mp`;
-    } else if (config.PICTURE.match(/[a-fA-F0-9]{32}/)) {
-        return `https://gravatar.com/avatar/${config.PICTURE}?s=512&d=mp`;
-    } else return config.PICTURE;
-}
-
 let body = new Vue({
     el: '.profile',
     data: {
         username: config.USERNAME,
-        profile_picture: getPicture(),
+        profile_picture: config.PICTURE,
         description: config.DESCRIPTION,
     }
 });
